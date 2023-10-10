4348c2d1dcf94e63586ad362d884a7261194416a
Carlos Fdez. Llamas <hello@sirikon.me>
2023-10-10 22:38:12 +0200

    wip

--- modules/scripts/publish.sh
+++ modules/scripts/publish.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 root="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../"
 docker compose \
   -f "${root}/docker-compose.yml" \
-  -f "${root}/modules/docker/docker-compose.yml" \
+  -f "${root}/modules/docker/docker-compose.base.yml" \
+  -f "${root}/modules/docker/docker-compose.publish.yml" \
   run --build sirikon-neocities ls -lah /
