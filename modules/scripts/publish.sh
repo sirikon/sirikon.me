22ab0336b6455745bc84573c206ce73b33ee20c1
Carlos Fdez. Llamas <hello@sirikon.me>
2023-10-12 14:35:59 +0200

    Use ekaitz's guile-neocities

--- modules/scripts/publish.sh
+++ modules/scripts/publish.sh
@@ -3,5 +3,16 @@ set -euo pipefail
 
 root="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../../"
 cd "$root"
+rm -rf ./output
 python -m sirikon-neocities build
-neocities push ./output
+
+# # Neocities official CLI
+# neocities push ./output
+
+# guile-neocities
+cd "./output"
+while IFS='\n' read -r line; do
+  echo "Uploading ${line:1}"
+  neocities upload "${line}" "${line:1}"
+  echo ""
+done <<< "$(find ./ -type f)"
