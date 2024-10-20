9d3ec9d13c5ffe955ff74fba15d3a75a31dccd61
Carlos Fdez. Llamas <hello@sirikon.me>
2024-10-20 12:01:00 +0200

    wip

--- meta/start.sh
+++ meta/start.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PYTHONPATH="$(pwd)/src/python"
+PYTHONPATH="$(pwd)/src"
 export PYTHONPATH
+exec ./.venv/bin/python -m flask \
+  --app sirikon_me.app \
+  run --port 8000 --reload
 
-function main() {
-  trap cleanup EXIT
-  log "Starting server on http://127.0.0.1:8000"
-  poetry run python -m sirikon_me watch &
-  poetry run python -m http.server --directory output --bind 127.0.0.1 8000 &
-  wait -n
-}
+# function main() {
+#   trap cleanup EXIT
+#   log "Starting server on http://127.0.0.1:8000"
+#   poetry run python -m sirikon_me watch &
+#   poetry run python -m http.server --directory output --bind 127.0.0.1 8000 &
+#   wait -n
+# }
 
-function cleanup() {
-  log "Cleaning up"
-  jobs -l
-  for job_pid in $(jobs -p); do
-    log "Killing ${job_pid}"
-    kill -9 "$job_pid" || true
-  done
-}
+# function cleanup() {
+#   log "Cleaning up"
+#   jobs -l
+#   for job_pid in $(jobs -p); do
+#     log "Killing ${job_pid}"
+#     kill -9 "$job_pid" || true
+#   done
+# }
 
-function log() {
-  printf "### [start.sh] %s\n" "$@"
-}
+# function log() {
+#   printf "### [start.sh] %s\n" "$@"
+# }
 
-main "$@"
+# main "$@"
